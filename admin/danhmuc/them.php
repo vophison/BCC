697cadd5b5f29877c5018087829f6118b7f470ea
vophison <phison.vps.19@gmail.com>
2020-12-30 14:55:04 +0700

    restore danhmuc

--- admin/danhmuc/them.php
+++ admin/danhmuc/them.php
@@ -2,6 +2,6 @@
 	 <div class="form-group col-sm-4"></div>
     <div class="form-group col-sm-3"><label class="m-auto" for="tendm">Tên Danh Mục</label><input type="text" class="form-control" name="tendm" autofocus></div>
     <div class="form-group col-sm-5"></div> <div class="form-group col-sm-4"></div>
-    <div class="form-group col-sm-3"><label for="masv">&emsp;</label><input type="submit" class="form-control badge-info" name="themdm" value="Thêm"></div>
+    <div class="form-group col-sm-3"><label for="masv">&emsp;</label><input type="submit" class="form-control badge-info" name="themdm" value="Thêm Danh Mục"></div>
     <hr>	
  </form><hr class=" badge-danger">
